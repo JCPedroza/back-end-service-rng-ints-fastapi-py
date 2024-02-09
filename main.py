348b449cd46d272f3a22e35727f7b8283fb94e87
JCPedroza <nemesis4go10@hotmail.com>
2024-02-08 22:07:42 -0600

    feat: make query params required

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI, HTTPException, Path, status
-from fastapi.responses import HTMLResponse
-
 from typing import Annotated
 from random import randrange
+from fastapi import FastAPI, HTTPException, Path, Query, status
+from fastapi.responses import HTMLResponse
 
-START = -99
-STOP = 100
 MIN_SIZE = 1
-MAX_SIZE = 100
-DOCS_PATH = "/docs"
-REDOCS_PATH = "/redocs"
+MAX_SIZE = 101
+MIN_VAL = -1_000_000
+MAX_VAL = 1_000_001
+OAPI_PATH = "/docs"
+RDOC_PATH = "/redocs"
 
-app = FastAPI(docs_url=DOCS_PATH, redoc_url=REDOCS_PATH)
+app = FastAPI(docs_url=OAPI_PATH, redoc_url=RDOC_PATH)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def get_root():
+    """Root page."""
+
     return f"""
     <!DOCTYPE html>
     <html>
@@ -24,8 +25,8 @@ async def get_root():
         </head>
         <body>
             <p>Docs at <ul>
-                <li><a href="{DOCS_PATH}">{DOCS_PATH}<a></li>
-                <li><a href="{REDOCS_PATH}">{REDOCS_PATH}<a></li>
+                <li><a href="{OAPI_PATH}">{OAPI_PATH}<a></li>
+                <li><a href="{RDOC_PATH}">{RDOC_PATH}<a></li>
             </ul></p>
         </body>
     </html>
@@ -34,20 +35,20 @@ async def get_root():
 
 @app.get("/rng/ints/{size}")
 async def get_rng_ints_SIZE(
-    size: Annotated[int, Path(ge=MIN_SIZE, le=MAX_SIZE)],
-    start: int = START,
-    stop: int = STOP,
+    size: Annotated[int, Path(ge=MIN_SIZE, lt=MAX_SIZE)],
+    start: Annotated[int, Query(ge=MIN_VAL, lt=MIN_VAL)],
+    stop: Annotated[int, Query(ge=MIN_VAL, lt=MIN_VAL)],
 ):
     """
-    Get a list of random integers.
+    Get list of random integers.
     - **size (path)**: Number of random integers that the list will contain.
     - **start (query)**: Minimum value of the random integers (inclusive).
     - **stop (query)**: Maximum value of the random integers (exclusive).
     """
-    if stop <= start:
+    if stop - start < 2:
         raise HTTPException(
             status.HTTP_422_UNPROCESSABLE_ENTITY,
-            f"stop must be greater than start (start:{start} stop:{stop})",
+            f"({stop=}) must be at least ({start=}) plus 2",
         )
 
     return {"ints": [randrange(start, stop) for _ in range(size)]}
